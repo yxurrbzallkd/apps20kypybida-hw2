bdcf7429e3fbe99c25fb8a0089b80b334e4dd332
yxurrbzallkd <kypybida@ucu.edu.ua>
2020-11-23 19:57:11 +0200

    ImmutableCollections initial commit

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,4 +1,5 @@
 package ua.edu.ucu.collections;
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
 
 public class Queue {
 	private ImmutableLinkedList queue;
--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,4 +1,6 @@
 package ua.edu.ucu.collections;
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 
 public class Stack {
 	private ImmutableLinkedList stack;
