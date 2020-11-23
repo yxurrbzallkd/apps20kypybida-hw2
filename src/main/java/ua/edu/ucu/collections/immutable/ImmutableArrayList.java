b17c813fea20967c9ac19ad5e83f10c5c0a0da7a
yxurrbzallkd <kypybida@ucu.edu.ua>
2020-11-23 20:49:52 +0200

    ImmutableCollections commit 17

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -15,7 +15,7 @@ public class ImmutableArrayList implements ImmutableList{
 
 	public ImmutableArrayList add(Object e) {
 		//adds element at the end of collection
-		Object[] newData = new Object[this.size()+1];
+		Object[] newData = new Object[this.size() + 1];
 		for (int i = 0; i < this.size(); i++) {
 			newData[i] = this.get(i); //
 		}
@@ -24,25 +24,25 @@ public class ImmutableArrayList implements ImmutableList{
 	}
     public ImmutableArrayList add(int index, Object e) throws IndexOutOfBoundsException{
 		//adds elemet at index, exception if index out of range
-		if ((index < 0) || (index >= this.size())){
+		if ((index < 0) || (index >= this.size())) {
 			throw new IndexOutOfBoundsException("can't add at index "+index);
 		}
-		Object[] newData = new Object[this.size()+1];
+		Object[] newData = new Object[this.size() + 1];
 		for (int i = 0; i < index; i++) {
 			newData[i] = this.get(i); //
 		}
 		newData[index] = e;
 		for (int i = index; i < this.size(); i++) {
-			newData[i+1] = this.get(i); //
+			newData[i + 1] = this.get(i); //
 		}
 		return new ImmutableArrayList(newData);
 	}
 
     public ImmutableArrayList addAll(Object[] c) {
 		//append array to collection
-		Object[] newData = Arrays.copyOf(this.data, this.size()+c.length);
+		Object[] newData = Arrays.copyOf(this.data, this.size() + c.length);
 		for (int i = 0; i < c.length; i++) {
-			newData[i+this.size()] = c[i]; //
+			newData[i + this.size()] = c[i]; //
 		}
 		return new ImmutableArrayList(newData);
 	}
@@ -51,9 +51,9 @@ public class ImmutableArrayList implements ImmutableList{
 		if ((index < 0) || (index >= this.size())) {
 			throw new IndexOutOfBoundsException("can't add at index "+index);
 		}
-		Object[] newData = Arrays.copyOf(this.data, this.size()+c.length-index);
+		Object[] newData = Arrays.copyOf(this.data, this.size() + c.length - index);
 		for (int i = index; i < c.length; i++) {
-			newData[i-index] = c[i];
+			newData[i - index] = c[i];
 		}
 		return new ImmutableArrayList(newData);
 	}
@@ -74,7 +74,7 @@ public class ImmutableArrayList implements ImmutableList{
 			newData[i] = this.get(i);
 		}
 		for (int i = index+1; i < this.size(); i++) {
-			newData[i-1] = this.get(i);
+			newData[i - 1] = this.get(i);
 		}
 		return new ImmutableArrayList(newData);
 	}
